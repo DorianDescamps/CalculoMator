c25359b417d0963032a7a76f54407f2cb27831be
DorianDescamps <dorian.descamps601@gmail.com>
2022-03-23 15:43:31 +0100

    Test dans SURVIVAL focntionnement boutons

---
+++ app/src/main/java/com/example/calculomator/menus/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.calculomator.menus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.example.calculomator.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button buttonplay = findViewById(R.id.button_play);
+        buttonplay.setOnClickListener(view -> openGamemodesActivity());
+    }
+
+    private void openGamemodesActivity() {
+        Intent intent = new Intent(this, GamemodesActivity.class);
+        startActivity(intent);
+    }
+}
