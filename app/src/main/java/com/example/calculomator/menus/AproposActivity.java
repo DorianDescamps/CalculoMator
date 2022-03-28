605e8422cbf0c06ff5e4ff87ff196f3f85103a82
DorianDescamps <dorian.descamps601@gmail.com>
2022-03-29 00:42:55 +0200

    Ajout page de crédits avec la toolbar dans le menu GamemodesActivity

--- app/src/main/java/com/example/calculomator/menus/AproposActivity.java
+++ app/src/main/java/com/example/calculomator/menus/AproposActivity.java
@@ -1,14 +1,15 @@
-//package com.example.calculomator.menus;
-//
-//import androidx.appcompat.app.AppCompatActivity;
-//
-//import android.os.Bundle;
-//
-//public class AproposActivity extends AppCompatActivity {
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_apropos);
-//    }
-//}
+package com.example.calculomator.menus;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.calculomator.R;
+
+import android.os.Bundle;
+
+public class AproposActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apropos);
+    }
+}
--- app/src/main/java/com/example/calculomator/menus/GamemodesActivity.java
+++ app/src/main/java/com/example/calculomator/menus/GamemodesActivity.java
@@ -1,9 +1,12 @@
 package com.example.calculomator.menus;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 
 import com.example.calculomator.R;
@@ -27,6 +30,20 @@ public class GamemodesActivity extends AppCompatActivity {
         Button buttonChrono = findViewById(R.id.button_chrono);
         buttonChrono.setOnClickListener(view -> openChronoActivity());
 
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==R.id.credit_menu_button){
+            Intent intent = new Intent(this, AproposActivity.class);
+            startActivity(intent);
+        }
+        return false;
     }
 
     private void openChronoActivity() {
