111c729598b203e59a0fac82163c57544752cc53
DorianDescamps <dorian.descamps601@gmail.com>
2022-03-28 21:36:24 +0200

    userList dans Score activity_register_score.xml

--- app/src/main/java/com/example/calculomator/menus/GamemodesActivity.java
+++ app/src/main/java/com/example/calculomator/menus/GamemodesActivity.java
@@ -10,7 +10,6 @@ import com.example.calculomator.R;
 import com.example.calculomator.gamemodes.ChronoActivity;
 import com.example.calculomator.gamemodes.ClassicActivity;
 import com.example.calculomator.gamemodes.InfiniteActivity;
-import com.example.calculomator.gamemodes.SurvivalActivity;
 
 public class GamemodesActivity extends AppCompatActivity {
 
@@ -28,20 +27,12 @@ public class GamemodesActivity extends AppCompatActivity {
         Button buttonChrono = findViewById(R.id.button_chrono);
         buttonChrono.setOnClickListener(view -> openChronoActivity());
 
-        Button buttonSurvival = findViewById(R.id.button_survival);
-        buttonSurvival.setOnClickListener(view -> openSurvivalActivity());
-
         Button buttonScores = findViewById(R.id.button_scores);
         buttonScores.setOnClickListener(view -> openScoresActivity());
     }
 
     private void openScoresActivity() {
         Intent intent = new Intent(this, ScoresActivity.class);
-        startActivity(intent);
-    }
-
-    private void openSurvivalActivity() {
-        Intent intent = new Intent(this, SurvivalActivity.class);
         startActivity(intent);
     }
 
